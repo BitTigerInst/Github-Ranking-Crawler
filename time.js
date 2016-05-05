4c3dd1037c375e04315a8e4bb8800362c42a6cfe
hackjustu <wenzhe89929@ucla.edu>
2016-05-04 19:26:54 -0700

    adapt the crawler to the Heroku server timezone

--- time.js
+++ time.js
@@ -1,13 +1,16 @@
-const Moment = require('moment');
+const Moment = require('moment-timezone');
 
 function get_current_timestamp() {
 
-    return Moment().format('YYYY-MM-DD HH:mm:ss Z');
+    return Moment().tz("America/Los_Angeles").format();
 }
 
 function get_current_date() {
 
     var now = new Date();
+    // The Heroku server is running in UTC+0. We adjust  the time zone to UTC-7.
+    new.setDate(new.getHours() - 7);
+
     return now.getFullYear() + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2);
 }
 
@@ -15,7 +18,9 @@ function get_current_date() {
 function get_last_week_date() {
 
     var oneWeekAgo = new Date();
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    // The Heroku server is running in UTC+0. We adjust  the time zone to UTC-7.
+    oneWeekAgo.setDate(oneWeekAgo.getHours() - 7);
 
     return oneWeekAgo.getFullYear() + "-" + ("0" + (oneWeekAgo.getMonth() + 1)).slice(-2) + "-" + ("0" + oneWeekAgo.getDate()).slice(-2);
 
