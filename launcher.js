3ab5cbf2d07184eb5f96f8f826d41ec963d4aee7
hackjustu <wenzhe89929@ucla.edu>
2016-05-09 15:36:57 -0700

    bug fixed

--- launcher.js
+++ launcher.js
@@ -12,14 +12,14 @@ app.get('/crawl', function (req, res) {
     var current_timestamp_h = Utils.get_current_timestamp_h();
     // We set Heroku scheduler to update the ranking at 6:30am PDT, 
     // so we won't accept any request outside 6~7 am.
-    if (current_timestamp_h != '6') {
+    if (current_timestamp_h == '6') {
+        GithubCrawler.crawl_github(true);
+    } else {
+        res.send('Busy crawling our github...');
         console.log('Warning!! A mystery request is captured...');
         console.log(req.headers);
         res.send('迷の请求……');
     }
-
-    GithubCrawler.crawl_github(true);
-    res.send('Busy crawling our github...');
 });
 
 app.get('/test', function (req, res) {
